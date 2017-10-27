c06f645ad99979ac87fecf1cc9cf1788ffbad00b
“Jose <jguti12@gmail.com>
2017-10-26 22:50:55 -0500

    added links to edit, delete, and add new car

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -21,5 +21,32 @@ class CarsController < ApplicationController
 	def show
 		@car = Car.find_by(id: params[:id])
 	end
+
+	def edit
+		@car = Car.find_by(id: params[:id])
+		render 
+	end
+
+	def update
+		@car = Car.find_by(id: params[:id])
+		# @car.assign_attributes(
+		# 											 year: params[:year],
+		# 											 make: params[:make],
+		# 											 model: params[:model],
+		# 											 color: params[:color])
+		# @car.save
+		@car.update(year: params[:year],
+								make: params[:make],
+								model: params[:model],
+								color: params[:color])
+		redirect_to '/'
+	end
+
+	def delete
+		@car = Car.find_by(id: params[:id])
+		@car.destroy
+
+		redirect_to '/'
+	end
 	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
 	post '/create_cars' => 'cars#create'
 
 	get '/cars/:id' => 'cars#show'
+
+	get '/cars/:id/edit' => 'cars#edit'
+
+	patch '/cars/:id' => 'cars#update'
+
+	delete '/cars/:id' => 'cars#delete'
 	
 end
